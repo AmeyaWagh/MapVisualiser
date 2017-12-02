846dc2063292187dd0643020c557a400f9af736b
AmeyaWagh <ameya555@gmail.com>
2017-12-02 02:39:01 -0500

    sanitized broadcaster

--- catkin_ws/src/using_markers/src/points_and_lines.cpp
+++ catkin_ws/src/using_markers/src/points_and_lines.cpp
@@ -29,7 +29,7 @@
 
 // %Tag(FULLTEXT)%
 #include <ros/ros.h>
-#include <visualization_msgs/Marker.h>
+// #include <visualization_msgs/Marker.h>
 #include <sensor_msgs/Range.h>
 #include <stdio.h>
 
@@ -39,7 +39,7 @@ int main( int argc, char** argv )
 {
   ros::init(argc, argv, "points_and_lines");
   ros::NodeHandle n;
-  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 10);
+  // ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 10);
   ros::Publisher sensor_pub = n.advertise<sensor_msgs::Range>("sensor_range", 10);
   ros::Rate r(30);
   
@@ -51,12 +51,12 @@ int main( int argc, char** argv )
   while (ros::ok())
   {
 // %Tag(MARKER_INIT)%
-    visualization_msgs::Marker points, line_strip, line_list;
-    points.header.frame_id = line_strip.header.frame_id = line_list.header.frame_id = "/my_frame";
-    points.header.stamp = line_strip.header.stamp = line_list.header.stamp = ros::Time::now();
-    points.ns = line_strip.ns = line_list.ns = "points_and_lines";
-    points.action = line_strip.action = line_list.action = visualization_msgs::Marker::ADD;
-    points.pose.orientation.w = line_strip.pose.orientation.w = line_list.pose.orientation.w = 1.0;
+    // visualization_msgs::Marker points, line_strip, line_list;
+    // points.header.frame_id = line_strip.header.frame_id = line_list.header.frame_id = "/my_frame";
+    // points.header.stamp = line_strip.header.stamp = line_list.header.stamp = ros::Time::now();
+    // points.ns = line_strip.ns = line_list.ns = "points_and_lines";
+    // points.action = line_strip.action = line_list.action = visualization_msgs::Marker::ADD;
+    // points.pose.orientation.w = line_strip.pose.orientation.w = line_list.pose.orientation.w = 1.0;
 // %EndTag(MARKER_INIT)%
 
     range_msg.radiation_type = sensor_msgs::Range::INFRARED;
@@ -66,61 +66,61 @@ int main( int argc, char** argv )
     range_msg.max_range = 0.4;   // For GP2D120XJ00F only. Adjust for other IR rangers
     range_msg.header.stamp = ros::Time::now();
 // %Tag(ID)%
-    points.id = 0;
-    line_strip.id = 1;
-    line_list.id = 2;
-// %EndTag(ID)%
+//     points.id = 0;
+//     line_strip.id = 1;
+//     line_list.id = 2;
+// // %EndTag(ID)%
 
-// %Tag(TYPE)%
-    points.type = visualization_msgs::Marker::POINTS;
-    line_strip.type = visualization_msgs::Marker::LINE_STRIP;
-    line_list.type = visualization_msgs::Marker::LINE_LIST;
-// %EndTag(TYPE)%
+// // %Tag(TYPE)%
+//     points.type = visualization_msgs::Marker::POINTS;
+//     line_strip.type = visualization_msgs::Marker::LINE_STRIP;
+//     line_list.type = visualization_msgs::Marker::LINE_LIST;
+// // %EndTag(TYPE)%
 
-// %Tag(SCALE)%
-    // POINTS markers use x and y scale for width/height respectively
-    points.scale.x = 0.2;
-    points.scale.y = 0.2;
+// // %Tag(SCALE)%
+//     // POINTS markers use x and y scale for width/height respectively
+//     points.scale.x = 0.2;
+//     points.scale.y = 0.2;
 
-    // LINE_STRIP/LINE_LIST markers use only the x component of scale, for the line width
-    line_strip.scale.x = 0.1;
-    line_list.scale.x = 0.1;
-// %EndTag(SCALE)%
+//     // LINE_STRIP/LINE_LIST markers use only the x component of scale, for the line width
+//     line_strip.scale.x = 0.1;
+//     line_list.scale.x = 0.1;
+// // %EndTag(SCALE)%
 
-// %Tag(COLOR)%
-    // Points are green
-    points.color.g = 1.0f;
-    points.color.a = 1.0;
+// // %Tag(COLOR)%
+//     // Points are green
+//     points.color.g = 1.0f;
+//     points.color.a = 1.0;
 
-    // Line strip is blue
-    line_strip.color.b = 1.0;
-    line_strip.color.a = 1.0;
+//     // Line strip is blue
+//     line_strip.color.b = 1.0;
+//     line_strip.color.a = 1.0;
 
-    // Line list is red
-    line_list.color.r = 1.0;
-    line_list.color.a = 1.0;
-// %EndTag(COLOR)%
+//     // Line list is red
+//     line_list.color.r = 1.0;
+//     line_list.color.a = 1.0;
+// // %EndTag(COLOR)%
 
 // %Tag(HELIX)%
     // Create the vertices for the points and lines
     float k=3.0;
     for (uint32_t i = 0; i < 100; ++i)
     {
-      float y = 5 * sin(f + i / 100.0f * 2 * M_PI);
-      float z = 5 * cos(f + i / 100.0f * 2 * M_PI);
+      // float y = 5 * sin(f + i / 100.0f * 2 * M_PI);
+      // float z = 5 * cos(f + i / 100.0f * 2 * M_PI);
 
-      geometry_msgs::Point p;
-      p.x = (int32_t)i - 50;
-      p.y = y;
-      p.z = z;
+      // geometry_msgs::Point p;
+      // p.x = (int32_t)i - 50;
+      // p.y = y;
+      // p.z = z;
 
-      points.points.push_back(p);
-      line_strip.points.push_back(p);
+      // points.points.push_back(p);
+      // line_strip.points.push_back(p);
 
-      // The line list needs two points for each line
-      line_list.points.push_back(p);
-      p.z += 1.0;
-      line_list.points.push_back(p);
+      // // The line list needs two points for each line
+      // line_list.points.push_back(p);
+      // p.z += 1.0;
+      // line_list.points.push_back(p);
       if(k>40.0)
       {
         k=3.0;
@@ -132,9 +132,9 @@ int main( int argc, char** argv )
     }
 // %EndTag(HELIX)%
 
-    marker_pub.publish(points);
-    marker_pub.publish(line_strip);
-    marker_pub.publish(line_list);
+    // marker_pub.publish(points);
+    // marker_pub.publish(line_strip);
+    // marker_pub.publish(line_list);
 
     r.sleep();
 
